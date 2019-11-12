26881d85c44b8bd7d6a0e173082c81eb1d3960ad
adamsbah <ballyadamsco@gmail.com>
2019-11-12 14:11:23 +0100

    ajout gender

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import axios from 'axios'
 
  class Characters extends Component {
@@ -6,24 +6,34 @@ import axios from 'axios'
          characters: []
         
      }
+     
+       
+    
      componentDidMount(){
-         axios.get(`https://harrypotterapi20.herokuapp.com/characters`)
-            .then(res=> {
-                console.log(res)
-                this.setState({characters: res.data })
-            })
+        axios.get(`https://harrypotterapi20.herokuapp.com/characters`)
+        .then(res=> {
+            console.log(res)
+            this.setState({characters: res.data })
+        })
+         
+            
      }
     render() {
         return (
-            <div>
+            <Fragment>
             <h1>Api d'Harry Pother</h1>
-            <ul>
-            {this.state.characters.map(character => <li key={character.id}>{character.name}</li>)}
-            </ul>
+                {this.state.characters.map(character => (<div key={character.id}>
+                    <img src= {character.image} alt={character.name} />
+                    <p>{character.gender}</p>
+                    <p>{character.eyeColour}</p>
+
+                    <p >{character.name}</p>
+                </div> )
+                 )}
             
             
                 
-            </div>
+            </Fragment>
         )
     }
 }
